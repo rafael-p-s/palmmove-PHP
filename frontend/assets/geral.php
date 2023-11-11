5a00f4a31ec7c382605ae5fccfb80b02501ae77d
rafael-p-s <rafaelsky321@gmail.com>
2023-11-11 12:34:33 -0300

    Configurado e criado as pastas e pgs.

---
+++ frontend/assets/geral.php
@@ -0,0 +1,22 @@
+<?php
+// Defina o cabeçalho como CSS para indicar que é um arquivo CSS
+header("Content-type: text/css");
+
+// Lista de arquivos CSS que você deseja combinar
+$cssFiles = array(
+    "./css/geral.css",
+    "./css/home/home.css",
+    "./css/admin/admin.css"
+);
+
+// Inicialize uma variável para armazenar o conteúdo CSS combinado
+$combinedCss = '';
+
+// Loop através da lista de arquivos e combine o conteúdo
+foreach ($cssFiles as $cssFile) {
+    $cssContent = file_get_contents($cssFile);
+    $combinedCss .= $cssContent;
+}
+
+// Saída do conteúdo CSS combinado
+echo $combinedCss;
---
+++ frontend/components/topHeader.php
@@ -0,0 +1,8 @@
+<?php
+$topHeader = "";
+$topHeader = $topHeader . 
+'
+    <div class="testeHeader">
+    <h1>Teste de explicação para o grupo.</h1>
+    </div>
+';
