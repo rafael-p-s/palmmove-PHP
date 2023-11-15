8ca2397c6bdfaba19d9ee623de86ce241f7ed94b
rafael-p-s <rafaelsky321@gmail.com>
2023-11-15 12:06:05 -0300

    Criado o documento de regras do projeto.

--- frontend/components/topHeader.php
+++ frontend/components/topHeader.php
@@ -5,19 +5,19 @@ $topHeader = $topHeader .
     <div class="testeHeader">
         <div class="logoHeader"></div>
         <div class="div_topHeader_buttons">
-            <button class="d_tH_btns">
+            <button class="div_tH_btns">
                 <span>Filmes</span>
             </button>
-            <button class="d_tH_btns">
+            <button class="div_tH_btns">
                 <span>Series</span>
             </button>
-            <button class="d_tH_btns">
+            <button class="div_tH_btns">
                 <span>Animes</span>
             </button>
         </div>
         <div class="div_tH_login">
             <img  src="./frontend/img/User.png" alt="Login">
-            <span class="span_cadastrar">Cadastrar</span>
+            <span class="tH_span_cadastrar">Cadastrar</span>
         </div>
     </div>
 ';
