e7f7f7d5b1980a0986daee2a4e78c54c38f28f8b
rafael-p-s <rafaelsky321@gmail.com>
2023-11-20 18:14:16 -0300

    Feito a tela de assistir filmes. Feito alteração no Header.

--- frontend/components/topHeader.php
+++ frontend/components/topHeader.php
@@ -2,22 +2,30 @@
 $topHeader = "";
 $topHeader = $topHeader .
     '
-    <div class="testeHeader">
-        <div class="logoHeader"></div>
-        <div class="div_topHeader_buttons">
-            <button class="div_tH_btns">
-                <span>Filmes</span>
-            </button>
-            <button class="div_tH_btns">
-                <span>Series</span>
-            </button>
-            <button class="div_tH_btns">
-                <span>Animes</span>
-            </button>
+    <header>
+    <section class="section_grid_main">
+      <div class="header_grid_logo">
+        <img src="../../img/LogoPalmMove2.png" alt="PalmMove">
+      </div>
+      <div class="header_grid_btns">
+        <button class="d_tH_btns">
+          <span>Filmes</span>
+        </button>
+        <button class="d_tH_btns">
+          <span>Series</span>
+        </button>
+        <button class="d_tH_btns">
+          <span>Animes</span>
+        </button>
+      </div>
+      <div class="header_grid_user">
+        <div>
+          <img src="../../img/User.png" alt="Logo">
+          <a href="">
+            <p style="margin-left: 5%;">Cadastrar</p>
+          </a>
         </div>
-        <div class="div_tH_login">
-            <img  src="./frontend/img/User.png" alt="Login">
-            <span class="tH_span_cadastrar">Cadastrar</span>
-        </div>
-    </div>
+      </div>
+    </section>
+  </header>
 ';
---
+++ frontend/pages/movies/filme.php
@@ -0,0 +1,28 @@
+<?php
+require('../../components/topHeader.php');
+require('../../components/blFooter.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../assets/geral.php">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo $topHeader;
+    ?>
+    
+    <footer>
+        <?php
+        echo $blFooter;
+        ?>
+    </footer>
+</body>
+
+</html>
